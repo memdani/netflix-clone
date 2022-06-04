940277b8b50403e50325d254d56be18a1df5ec70
memdani <memdaniy12@gmail.com>
2022-06-04 14:18:08 +0530

    day 2 scroll changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ const Home= ({netflixOriginals,
     const showModal =useRecoilValue(modalState)
 
   return (
-    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
+    <div className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${showModal && '!h-screen overflow-hidden'}`}>
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
